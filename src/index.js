32dfac200f5a3804ad8ca1b2105ff3ad5c726b5a
Jacob Childs <crisko64@gmail.com>
2026-03-02 08:00:50 -0600

    Created form for task creation

--- src/index.js
+++ src/index.js
@@ -7,9 +7,66 @@ const newProject = new project('work', 'these are the things I have to do for wo
 const newItem = new toDoItem('get money', 'b', '01/01/2019', '1', 'none');
 console.log(newItem);
 
-
 const contentDiv = document.getElementById('content'); // this will display the project and it's todo items.
-const sidebar = document.createElement('div'); //sidebar that displays separate projects/task areas. Can click on one to switch to that project's view
+const projectsDiv = document.getElementById('projects-view');
+const projectContainer = document.createElement('div');
+
+// creating dialog and form for making tasks.
+const formDialog = document.createElement('dialog');
+const taskDiv = document.createElement('div');
+taskDiv.id = "taskDiv";
+
+const taskForm = document.createElement('form');
+// title, descr, dueDate, priority, notes
+const titleLabel = document.createElement('label');
+titleLabel.textContent = "Task title: ";
+titleLabel.for = "taskTitle";
+const titleInput = document.createElement('input');
+titleInput.id = "taskTitle";
+titleInput.name = "taskTitle";
+titleInput.type = "text";
+const descrLabel = document.createElement('label');
+descrLabel.textContent = "Task Description: ";
+descrLabel.for = "taskDescr";
+const descrInput = document.createElement('input');
+descrInput.id = "taskDescr";
+descrInput.name = "taskDescr";
+descrInput.type = "text";
+const dueDateLabel = document.createElement('label');
+dueDateLabel.textContent = "Due Date: ";
+dueDateLabel.for = "taskDueDate";
+const dueDateInput = document.createElement('input');
+dueDateInput.id = "taskDueDate";
+dueDateInput.name = "taskDueDate";
+dueDateInput.type = "date";
+const priorityLabel = document.createElement('label');
+priorityLabel.textContent = "Priority Level (1-5): ";
+priorityLabel.for = "priorityLevel";
+const priorityInput = document.createElement('input');
+priorityInput.id = "priorityLevel";
+priorityInput.name = "priorityLevel";
+priorityInput.text = "number"
+priorityInput.min = 1;
+priorityInput.max = 5;
+const formSubmitLabel = document.createElement('btn');
+formSubmitLabel.textContent = "Submit Form";
+formSubmitLabel.for = "submitForm";
+const formSubmitBtn = document.createElement('input');
+formSubmitBtn.id = "submitForm";
+formSubmitBtn.name = "submitForm";
+formSubmitBtn.type = "submit";
+
+//making dialog + form connections
+taskForm.append(titleLabel, titleInput, descrLabel, descrInput, dueDateLabel, dueDateInput, priorityLabel, priorityInput, formSubmitLabel, formSubmitBtn);
+formDialog.append(taskForm);
+taskDiv.append(formDialog);
+
+// grab createTaskBtn and attach event listener 
+const createTaskBtn = document.getElementById('create-task-btn');
+createTaskBtn.addEventListener('click', () => {
+
+});
+
 
 
 
--- src/tasks.js
+++ src/tasks.js
@@ -11,9 +11,7 @@ export class toDoItem {
         this.notes = notes;
         let status = "incomplete";
     }
-
 };
-
 export class project {
     constructor(projectName, descr){
         const toDoArray = [];
